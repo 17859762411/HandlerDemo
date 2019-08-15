aee702dd2e69a6fa2abb6a9e1bfe785bf593cc1a
LvTong <2293983846@qq.com>
2019-08-15 18:49:22 +0800

    init

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
 r
 Bapp/src/main/java/com/android/lvtong/handlerdemo/MainActivity.java,2\b\2b059bc008be704d921c6a4364c0f02f77fad78a
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+s
+Capp/src/main/java/com/android/lvtong/handlerdemo/ThirdActivity.java,b\4\b45b55cbdff8ec19a9f0c04094808262b6d77dac
+t
+Dapp/src/main/java/com/android/lvtong/handlerdemo/SecondActivity.java,6\7\67861d069cd2ff8e089c8d0dfdc37019f88d5a1d
--- app/src/main/java/com/android/lvtong/handlerdemo/MainActivity.java
+++ app/src/main/java/com/android/lvtong/handlerdemo/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.lvtong.implementationlibary.DialogOption;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler mExampleHandler;
 
+    private static final int TIME = 10;
+
     /** 方法一，通过Handler + Message的方式实现倒计时。 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +32,24 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_jump_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
         findViewById(R.id.btn_jump_third).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,ThirdActivity.class));
+                startActivity(new Intent(MainActivity.this, ThirdActivity.class));
             }
         });
+        findViewById(R.id.btn_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogOption.with(MainActivity.this)
+                            .create()
+                            .show();
+            }
+        });
+
 
         tvTime = findViewById(R.id.tv_time);
 
@@ -44,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         btnTime1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 1; i <= 10; i++) {
+                for (int i = 1; i <= TIME; i++) {
                     Message message = Message.obtain(mExampleHandler);
                     message.what = 10 - i;
                     //通过延迟发送消息，每隔一秒发送一条消息
@@ -52,26 +65,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
         mExampleHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                tvTime.setText(msg.what + "");   //在handleMessage中处理消息队列中的消息
+                //在handleMessage中处理消息队列中的消息
+                tvTime.setText(String.valueOf(msg.what));
             }
         };
-        /** 方法二，通过Handler + Runnable的方式实现倒计时。 */
+
+        /* 方法二，通过Handler + Runnable的方式实现倒计时。*/
         Button btnTime2 = findViewById(R.id.btn_time2);
         btnTime2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 1; i <= 10; i++) {
+                for (int i = 1; i <= TIME; i++) {
                     final int fadedSecond = i;
                     //每延迟一秒，发送一个Runnable对象
                     mExampleHandler = new Handler();
                     mExampleHandler.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            tvTime.setText((10 - fadedSecond) + "");
+                            tvTime.setText(String.valueOf(10 - fadedSecond));
                         }
                     }, 1000 * i);
                 }
